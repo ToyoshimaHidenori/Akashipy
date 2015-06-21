1634c49d304b216c24d85ef0b03aed101f3226c3
aoichi <sosososososochan@gmail.com>
2015-06-21 16:09:52 +0900

    file書き込み

--- StudyMeetingCalender.py
+++ StudyMeetingCalender.py
@@ -7,6 +7,11 @@ r = requests.get(url + keyword)
 
 print(r.status_code, r.headers['content-type'])
 
+f = open('hoge.txt', 'w')
+
 for event in r.json()['events'][:10]:
-    print(event['event_id'] , event['title'],'\n開始時間 : ', event['started_at'] + '\n')
+    print(event['event_id'], event['title'],'\n開始時間 : ', event['started_at'] + '\n')
+    f.write("%d %s\n" % (event['event_id'], event['title']))
+
+f.close()
 
