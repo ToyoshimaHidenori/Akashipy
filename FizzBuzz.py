853e50c0ef9098c7b2959eaf05d33cba64bab050
ToyoshimaHidenori <teshima.hidenori@icloud.com>
2015-06-21 14:27:30 +0900

    5の倍数の時Buzz表示

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,11 @@
 
 for i in range(100):
-    if i % 3 == 0:
+    if i == 0:
+        print(0)
+    elif i % 3 == 0:
         print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
     else:
         print(i)
 
