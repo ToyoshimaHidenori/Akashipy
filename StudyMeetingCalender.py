bf70ba13e819595b8db7094711cfcab8ae84cbb2
ToyoshimaHidenori <teshima.hidenori@icloud.com>
2015-06-21 15:45:22 +0900

    検索窓追加

--- StudyMeetingCalender.py
+++ StudyMeetingCalender.py
@@ -1,6 +1,7 @@
 import requests
 
 url = 'http://connpass.com/api/v1/event/?keyword='
+print('イベントを検索:',end='')
 keyword = input()
 r = requests.get(url + keyword)
 
@@ -8,3 +9,4 @@ print(r.status_code, r.headers['content-type'])
 
 for event in r.json()['events'][:10]:
     print(event['event_id'] , event['title'],'\n開始時間 : ', event['started_at'] + '\n')
+
